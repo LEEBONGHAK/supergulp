dfa49d0a82378c2d37445e36e98efd18811ddc38
octo <dlqhdgkr456@gmail.com>
2021-08-11 22:16:51 +0900

    1.1 Pug Task part Two

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,6 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";         // pug 파일을 html로 변환 시키는 데 사용하는 라이브러리
+import del from "del";               // 이미 build된 내용을 삭제하기 위한 라이브러리
 
 const routes = {
     pug: {
@@ -8,10 +9,16 @@ const routes = {
     }
 };
 
-export const pug = () => 
+const pug = () => 
     gulp
         .src(routes.pug.src)
         .pipe(gpug())
         .pipe(gulp.dest(routes.pug.dest));         // gulp은 pipe(데이터/파일 stream을 만드는 것)와 함께 쓰임
 
-export const dev = gulp.series([pug]);
+const clean = () => del(["build"]);
+
+const prepare = gulp.series([clean])
+
+const assets = gulp.series([pug])
+
+export const dev = gulp.series([prepare, assets]);      // export는 pakage.json에서 쓸 command만 해주면 됨
