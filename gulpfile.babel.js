3b3a7f84966a62f162bc754c5dd7cddae23f44ff
octo <dlqhdgkr456@gmail.com>
2021-08-11 21:46:49 +0900

    1.0 Pug Task part One

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,17 @@
 import gulp from "gulp";
+import gpug from "gulp-pug";         // pug 파일을 html로 변환 시키는 데 사용하는 라이브러리
+
+const routes = {
+    pug: {
+        src: "src/*.pug",            // src: "src/**/*.pug" : 폴더 안쪽의 파일까지 변환 시키고 싶을 경우 사용
+        dest: "build"
+    }
+};
+
+export const pug = () => 
+    gulp
+        .src(routes.pug.src)
+        .pipe(gpug())
+        .pipe(gulp.dest(routes.pug.dest));         // gulp은 pipe(데이터/파일 stream을 만드는 것)와 함께 쓰임
+
+export const dev = gulp.series([pug]);
